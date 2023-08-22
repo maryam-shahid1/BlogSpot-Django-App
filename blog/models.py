8c91ff4f6037225c576d96e61ddd3f4548919188
maryam-shahid1 <maryam.shahid@arbisoft.com>
2023-08-22 12:41:28 +0500

    Added models

---
+++ blog/models.py
@@ -0,0 +1,46 @@
+"""
+This module contains post and comment models.
+"""
+
+from django.db import models
+from django.utils import timezone
+
+from user.models import Organisation, User
+
+
+class Post(models.Model):
+    PostStatusChoices = [
+        ('Draft', 'Draft'),
+        ('Pending', 'Pending'),
+        ('Approved', 'Approved'),
+        ('Rejected', 'Rejected'),
+    ]
+
+    CategoryChoices = [
+        ('Technology', 'Technology'),
+        ('Lifestyle', 'Lifestyle'),
+        ('Food', 'Food'),
+    ]
+
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    status = models.CharField(
+        max_length=100,
+        choices=PostStatusChoices,
+        default='Draft')
+    published_date = models.DateTimeField(default=timezone.now())
+    slug = models.SlugField(unique=True)
+    category = models.CharField(
+        max_length=50,
+        choices=CategoryChoices,
+        default='')
+    is_public = models.BooleanField(default=False)
+    organisation = models.ForeignKey(Organisation, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
+class Comment(models.Model):
+    content = models.TextField()
+    date = models.DateTimeField(auto_now_add=True)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
--- user/models.py
+++ user/models.py
@@ -1,16 +1,16 @@
 """
-This module contains custom user model and manager.
+This module contains custom user and organisation's models.
 """
 
 from django.contrib.auth.models import (AbstractBaseUser, BaseUserManager,
                                         PermissionsMixin)
 from django.db import models
+from django.utils import timezone
 
 
 class CustomUserManager(BaseUserManager):
 
-    def create_user(self, email, password=None,
-                    is_superuser=False, **extra_fields):
+    def create_user(self, email, password=None, **extra_fields):
 
         if not email:
             raise ValueError("Email value must be set!")
@@ -18,35 +18,73 @@ class CustomUserManager(BaseUserManager):
         email = self.normalize_email(email)
         user = self.model(email=email, **extra_fields)
         user.set_password(password)
-        user.is_superuser = is_superuser
         user.save(using=self._db)
+
         return user
 
     def create_superuser(self, email, password=None, **extra_fields):
         extra_fields.setdefault("is_staff", True)
         extra_fields.setdefault("is_superuser", True)
+        extra_fields.setdefault("role", 'Admin')
+        extra_fields.setdefault("request_status", 'Accepted')
 
         if extra_fields.get("is_staff") is not True:
             raise ValueError("Superuser must have is_staff=True.")
         if extra_fields.get("is_superuser") is not True:
             raise ValueError("Superuser must have is_superuser=True.")
 
-        return self.create_user(email, password, **extra_fields)
+        organisation = extra_fields.get("organisation")
+
+        user = self.create_user(email, password, **extra_fields)
+
+        if organisation:
+            user.organisation = organisation
+            user.save(using=self._db)
+
+        return user
+
+
+class Organisation(models.Model):
+    org_name = models.CharField(max_length=100)
+    website = models.URLField(max_length=200, blank=True, null=True)
 
 
 class User(AbstractBaseUser, PermissionsMixin):
 
+    UserRoleChoices = [
+        ('Admin', 'Admin'),
+        ('Author', 'Author'),
+    ]
+
+    RequestStatusChoices = [
+        ('Pending', 'Pending'),
+        ('Accepted', 'Accepted'),
+        ('Rejected', 'Rejected'),
+    ]
+
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
     email = models.EmailField(unique=True)
-    organisation = models.CharField(max_length=100)
+    username = models.CharField(max_length=20, unique=True)
+    date_joined = models.DateTimeField(default=timezone.now())
+    organisation = models.ForeignKey(
+        Organisation,
+        on_delete=models.CASCADE
+    )
+    request_status = models.CharField(
+        max_length=100,
+        choices=RequestStatusChoices
+    )
+    role = models.CharField(
+        max_length=100,
+        choices=UserRoleChoices,
+        default='Author'
+    )
 
-    is_student = models.BooleanField(default=False)
-    is_organisation = models.BooleanField(default=False)
     is_staff = models.BooleanField(default=False)
     is_superuser = models.BooleanField(default=False)
 
     objects = CustomUserManager()
 
-    USERNAME_FIELD = "email"
-    REQUIRED_FIELDS = []
+    USERNAME_FIELD = 'username'
+    REQUIRED_FIELDS = ['email']
